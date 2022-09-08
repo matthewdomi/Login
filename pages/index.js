67853113b34cc0f24072a5c82f042fd7579e5be8
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-09-08 10:22:33 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import SignIn from "./components/Form/sign-in"
+import ExpandedNavbar from "./components/Navbar/expandNavbar"
+import StraightNavbar from "./components/Navbar/straightNavbar"
 
-import Accordion from '../components/Accordion';
-import Navbar from '../components/Navbar';
-
-import styles from '../styles/Home.module.css'
-
-import { FaFacebook, FaLinkedIn, FaGoogle, FaYoutube, FaTelegram, FaRegEnvelope } from "react-icons/fa";
-import { MdLockOutline } from "react-icons/md"
 
 
 
 export default function Home() {
   return (
     <>
-    <Navbar />
+    {/* <ExpandedNavbar/> */}
+    <SignIn />
+
+    
     
 
     </>
